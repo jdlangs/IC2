45dec1bce607565c3aba33b7e9484775113088a1
geoffreychiou <geoffrey.chiou@gmail.com>
2017-05-31 13:30:07 -0500

    restructured targets

--- industrial_calibration_libs/include/industrial_calibration_libs/targets.h
+++ industrial_calibration_libs/include/industrial_calibration_libs/targets.h
@@ -6,13 +6,6 @@
 namespace industrial_calibration_libs
 {
 
-struct Point3D
-{
-  double x;
-  double y;
-  double z;
-};
-
 enum target_types
 {
   ChessBoard = 0;
@@ -20,69 +13,62 @@ enum target_types
   ModifiedCircleGrid = 2;
 };
 
+struct Point3D
+{
+  double x;
+  double y;
+  double z;
+};
+
+struct TargetDefinition
+{
+  std::string target_name;
+  std::string target_frame;
+  std::string transform_interface;
+  
+  double angle_axis_ax;
+  double angle_axis_ay;
+  double angle_axis_az;
+  
+  double position_x;
+  double position_y;
+  double position_z;
+
+  std::size_t traget_type;
+
+  std::size_t num_points;
+  std::size_t num_rows;
+  std::size_t num_cols;
+
+  std::vector<Point3d> points;
+  double circle_diameter;
+};
+
 class Target
 {
 public:
-  Target();
-
-  Target(std::string target_name, std::string target_frame, std::string transform_interface,
-    double angle_axis_ax = 0.0, double angle_axis_ay = 0.0, double angle_axis_az = 0.0,
-    double position_x = 0.0, double position_y = 0.0, double position_z = 0.0);
-
-  bool generatePoints(double spacing);
 
 protected:
-  std::string target_name_;
-  std::string target_frame_;
-  std::string transform_interface_; 
-  double angle_axis_ax_;
-  double angle_axis_ay_;
-  double angle_axis_az_;
-  double position_x_;
-  double position_y_;
-  double position_z_;
-  std::size_t num_points;
-
-  std::vector<Point3D> points_;
+  TargetDefinition target_definition_;
+  std::vector<Point3d> points_;
 };
 
 class CheckerBoardTarget : public Target
 {
 public:
-  CheckerBoardTarget(const std::string file_path);
-
-  CheckerBoardTarget(std::string target_name, std::string target_frame, 
-    std::string transform_interface, double angle_axis_ax = 0.0, double angle_axis_ay = 0.0, 
-    double angle_axis_az = 0.0, double position_x = 0.0, double position_y = 0.0, 
-    double position_z = 0.0, std::size_t num_rows, std::size_t num_cols);
-
 
 protected:
-  bool loadCheckerBoardTarget(const std::string file_path);
-  std::size_t num_rows_;
-  std::size_t num_cols_;
 };
 
 class CircleGridTarget : public Target
 {
 public:
-  CircleGridTarget(const std::string &file_path);
-
-  CircleGridTarget(std::string target_name, std::string target_frame, std::string transform_interface, 
-    double angle_axis_ax = 0.0, double angle_axis_ay = 0.0, double angle_axis_az = 0.0,
-    double position_x = 0.0, double position_y = 0.0, double position_z = 0.0, 
-    std::size_t num_rows, std::size_t num_cols, bool is_symmetric, double circle_diameter);
+  CircleGridTarget(const std::string yaml_path);
 
 
 protected:
-  bool loadCircleGridTarget(const std::string &file_path);
-  std::size_t num_rows_;
-  std::size_t num_cols_;
   bool is_symmetric_;
-  double circle_diameter_;
 };
-  
-} // namespace industrial_calibration_libs
 
 #endif // TARGETS_H
 
