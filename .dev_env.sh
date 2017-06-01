a4b8a27c7b08e1e6ae46a87e1bf6b349c94a44fa
geoffreychiou <geoffrey.chiou@gmail.com>
2017-06-01 14:35:56 -0500

    added a launcher for my editor

---
+++ .dev_env.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# This file exists so I can launch my editor with all the files at once
+# since I move around to a lot of different computers. I am pretty much
+# the only person working on this project for now so I'm going to leave 
+# this here and remove it when I'm done.
+
+if [ "$1" = "all" ]; then
+  subl industrial_calibration_libs/CMakeLists.txt
+  subl industrial_calibration_libs/package.xml
+  subl industrial_calibration_libs/src/*.cpp
+  subl industrial_calibration_libs/include/industrial_calibration_libs/*.h
+  subl industrial_calibration_libs/test/*.cpp
+  subl industrial_calibration_libs/test/*.h
+elif [ "$1" = "code" ]; then
+  subl industrial_calibration_libs/src/*.cpp
+  subl industrial_calibration_libs/include/industrial_calibration_libs/*.h
+elif [ "$1" = "config" ]; then
+  subl industrial_calibration_libs/CMakeLists.txt
+  subl industrial_calibration_libs/package.xml
+elif [ "$1" = "test" ]; then
+  subl industrial_calibration_libs/test/*.cpp
+  subl industrial_calibration_libs/test/*.h
+else
+  subl industrial_calibration_libs/CMakeLists.txt
+  subl industrial_calibration_libs/package.xml
+  subl industrial_calibration_libs/src/*.cpp
+  subl industrial_calibration_libs/include/industrial_calibration_libs/*.h
+  subl industrial_calibration_libs/test/*.cpp
+  subl industrial_calibration_libs/test/*.h
+fi
