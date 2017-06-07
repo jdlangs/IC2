b52a5064d16faa8a9de6c760395282984ba091b7
geoffreychiou <geoffrey.chiou@gmail.com>
2017-06-07 00:09:17 -0500

    Added class definition for observation related stuff

--- industrial_calibration_libs/include/industrial_calibration_libs/observations.h
+++ industrial_calibration_libs/include/industrial_calibration_libs/observations.h
@@ -1,8 +1,42 @@
 #ifndef OBSERVATIONS_H
 #define OBSERVATIONS_H
 
+#include <industrial_calibration_libs/targets.h>
+
 namespace industrial_calibration_libs
 {
+struct Observation
+{
+  std::shared_ptr<Target> target;
+  int point_id;
+  double point_x;
+  double point_y;
+  // Note(gChiou): May need cost function type associated with observation and
+  // an 'intermediate_frame'. Leaving it out for now.
+  // see: https://github.com/ros-industrial/industrial_calibration/blob/kinetic-devel/industrial_extrinsic_cal/include/industrial_extrinsic_cal/camera_observer.hpp
+};
+
+typedef std::vector<Observation> CameraObservations;
+
+// Note(gChiou): I think the above typedef is for a single image.
+// The input of the ObservationExtractor class should be a vector of 
+// all images containing a target and the output should be a vector of:
+// typedef std::vector<CameraObservations> ObservationData 
+// (or something like that) of the same size as input vector of images.
+// Something to test for.
+typedef std::vector<CameraObservations> ObservationData;
+
+class ObservationExtractor
+{
+public:
+  ObservationExtractor(void);
+
+  ~ObservationExtractor(void);
+
+  bool getObservations(CameraObservations &observations);
+
+private:
+};
 
 } // namespace industrial_calibration_libs
 #endif // OBSERVATIONS_H
---
+++ industrial_calibration_libs/src/observations.cpp
@@ -0,0 +1,7 @@
+#include <industrial_calibration_libs/observations.h>
+
+namespace industrial_calibration_libs
+{
+
+
+} // namespace industrial_calibration_libs
