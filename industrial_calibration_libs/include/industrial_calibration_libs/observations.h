56fcd47949510e5a8437eff460e7caa52cee7fa1
geoffreychiou <geoffrey.chiou@gmail.com>
2017-06-15 18:47:56 -0500

    Added highgui include to observations

--- industrial_calibration_libs/include/industrial_calibration_libs/observations.h
+++ industrial_calibration_libs/include/industrial_calibration_libs/observations.h
@@ -6,6 +6,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 namespace industrial_calibration_libs
 {
