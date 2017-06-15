53efb01b9a584a4cda1b1bbb29c49fe64a4bbc5c
geoffreychiou <geoffrey.chiou@gmail.com>
2017-06-15 17:31:17 -0500

    added template code for observation tests

---
+++ industrial_calibration_libs/test/test_observations.cpp
@@ -0,0 +1,14 @@
+#include <industrial_calibration_libs/industrial_calibration_libs.h>
+#include <gtest/gtest.h>
+#include "test_utils.h"
+
+TEST(Observations, load_observations)
+{
+
+}
+
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
