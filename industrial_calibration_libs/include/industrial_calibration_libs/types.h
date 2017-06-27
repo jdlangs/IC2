096def4b18539cd0312a7a41d4db3b85672d3371
geoffreychiou <geoffrey.chiou@gmail.com>
2017-06-27 16:42:34 -0500

    Added Pose6D definition and implementation

--- industrial_calibration_libs/include/industrial_calibration_libs/types.h
+++ industrial_calibration_libs/include/industrial_calibration_libs/types.h
@@ -3,6 +3,10 @@
 
 #include <vector>
 #include <cassert>
+#include <limits>
+
+#include <ceres/rotation.h>
+#include <Eigen/Dense>
 
 namespace industrial_calibration_libs
 {
@@ -29,6 +33,76 @@ struct Point3D
   double x;
   double y;
   double z;
+};
+
+// TODO(gChiou): Refactor this and remove un-needed code.
+class Pose6D
+{
+public:
+  Pose6D(double tx, double ty, double tz, double aax, double aay, double aaz);
+
+  Pose6D();
+
+  // void setBasis( tf::Matrix3x3 & m);
+  void setBasis(Eigen::Matrix3d &m);
+
+  // void setOrigin(tf::Vector3 & v);
+  void setOrigin(Eigen::Vector3d &v);
+
+  void setOrigin(double tx, double ty, double tz);
+
+  void setEulerZYX(double ez, double ey, double ex);
+
+  void setQuaternion(double qx, double qy, double qz, double qw);
+
+  void setAngleAxis(double aax, double aay, double aaz);
+
+  // tf::Matrix3x3 getBasis() const;
+  Eigen::Matrix3d getBasis(void) const;
+
+  void getEulerZYX(double &ez, double &ey, double &ex) const;
+
+  // tf::Vector3 getOrigin() const;
+  Eigen::Vector3d getOrigin(void) const;
+
+  void getQuaternion(double &qx, double &qy, double &qz, double &qw) const;
+
+  Pose6D getInverse(void) const;
+
+  // void show(std::string message);
+
+  Pose6D operator *(Pose6D pose2) const;
+
+  // union
+  // {
+  //   struct
+  //   {
+  //     double ax; 
+  //     double ay; 
+  //     double az; 
+  //     double x;  
+  //     double y;  
+  //     double z;  
+  //   };
+
+  //   struct
+  //   {
+  //     double pb_aa[3]; 
+  //     double pb_loc[3];
+  //   };
+
+  //   struct
+  //   {
+  //     double pb_pose[6]; 
+  //   };
+  // }; 
+
+  double ax; 
+  double ay; 
+  double az; 
+  double x;  
+  double y;  
+  double z;   
 };
 
 } // namespace industrial_calibration_libs
