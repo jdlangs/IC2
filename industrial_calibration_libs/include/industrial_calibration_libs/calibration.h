56895e692208c907072d5d770d38b7976f54e34c
geoffreychiou <geoffrey.chiou@gmail.com>
2017-06-28 13:44:15 -0500

    added temporary IntrinsicCalibration class

--- industrial_calibration_libs/include/industrial_calibration_libs/calibration.h
+++ industrial_calibration_libs/include/industrial_calibration_libs/calibration.h
@@ -1,11 +1,65 @@
 #ifndef CALIBRATION_H
 #define CALIBRATION_H
 
-#include <industrial_calibration_libs/targets.h>
 #include <industrial_calibration_libs/cost_functions.h>
+#include <industrial_calibration_libs/observations.h>
+#include <industrial_calibration_libs/targets.h>
 
 namespace industrial_calibration_libs
 {
+// Note(gChiou): Going to leave this out for now and focus on getting intrinsic
+// calibration to work.
+
+// class CalibrationJob
+// {
+// public:
+//   CalibrationJob(const ObservationData &observation_data, const Target &target);
+
+//   ~CalibrationJob();
+
+//   virtual bool calibrate();
+
+// protected:
+//   ObservationData observation_data_;
+//   Target target_;
+// };
+
+// class CalibrateCameraOnWristStaticTargetExtrinsic : public CalibrationJob
+// {
+// public:
+// private:
+//   struct Results
+//   {
+
+//   };
+// };
+
+// class CalibrateCameraOnWristStaticTargetIntrinsic : public CalibrationJob
+// {
+// public:
+// private:
+//   struct Results
+//   {
+
+//   };
+// };
+
+// Note(gChiou): This is temporary
+class IntrinsicCalibration
+{
+public:
+  IntrinsicCalibration(const ObservationData &observation_data, const Target &target,
+    const std::vector<Pose6D> link_poses);
+
+  ~IntrinsicCalibration(void);
+
+  bool Calibrate(void);
+
+private:
+  ObservationData observation_data_;
+  Target target_;
+  std::vector<Pose6D> link_poses_;
+};
 
 } // namespace industrial_calibration_libs
 
--- industrial_calibration_libs/src/calibration.cpp
+++ industrial_calibration_libs/src/calibration.cpp
@@ -3,5 +3,59 @@
 
 namespace industrial_calibration_libs
 {
+IntrinsicCalibration::IntrinsicCalibration(const ObservationData &observation_data, const Target &target, const std::vector<Pose6D> link_poses) : 
+  observation_data_(observation_data), target_(target), link_poses_(link_poses) { }
+
+bool IntrinsicCalibration::Calibrate(void)
+{
+  ceres::Problem problem;
+
+  std::size_t num_images = observation_data_.size();
+
+  // Check if number of observations per image is consistent.
+  std::size_t observations_per_image;
+  for (std::size_t i = 0; i < num_images; i++)
+  {
+    if (i == 0) {observations_per_image = observation_data_[i].size();}
+    else
+    {
+      if (observations_per_image != observation_data_[i].size()) {return false;}
+    }
+  }
+
+  // Iterate through every observation image.
+  for (std::size_t i = 0; i < num_images; i++)
+  {
+    double extrinsics[6];
+    double intrinsics[9];
+    double target_to_world[6];
+
+    // Iterate through every observation in the observation image.
+    for (std::size_t j = 0; j < observations_per_image; j++)
+    {
+      double observed_x = observation_data_[i][j].x;
+      double observed_y = observation_data_[i][j].y;
+      Pose6D link_pose = link_poses_[i];
+      Point3D point = target_.getData()->points[0];
+
+      ceres::CostFunction* cost_function = 
+        CameraOnWristStaticTargetIntrinsic::Create(observed_x, observed_y, 
+          link_pose, point);
+
+      problem.AddResidualBlock(cost_function, NULL, extrinsics, 
+        intrinsics, target_to_world);
+    }
+  }
+
+  // Solve
+  ceres::Solver::Options options;
+  ceres::Solver::Summary summary;
+  options.linear_solver_type = ceres::DENSE_SCHUR;
+  options.minimizer_progress_to_stdout = true;
+  options.max_num_iterations = 2000;
+  ceres::Solve(options, &problem, &summary);
+
+  return false; // REPLACE THIS
+}
 
 } // namespace industrial_calibration_libs
