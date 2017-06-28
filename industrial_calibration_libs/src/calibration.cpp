69bed38fffe335e3ca5fb60a3008edac56a4eb8c
geoffreychiou <geoffrey.chiou@gmail.com>
2017-06-28 12:36:25 -0500

    added namespace

--- industrial_calibration_libs/src/calibration.cpp
+++ industrial_calibration_libs/src/calibration.cpp
@@ -1,2 +1,7 @@
 #include <industrial_calibration_libs/cost_functions.h>
 #include <industrial_calibration_libs/calibration.h>
+
+namespace industrial_calibration_libs
+{
+
+} // namespace industrial_calibration_libs
