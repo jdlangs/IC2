0e6dba35e742cef8251f3c8309f4717054fcb316
geoffreychiou <geoffrey.chiou@gmail.com>
2017-07-19 16:32:38 -0500

    Boilerplate ros code

--- industrial_calibration/include/industrial_calibration/cal_data_collector.h
+++ industrial_calibration/include/industrial_calibration/cal_data_collector.h
@@ -1,1 +1,22 @@
+#ifndef CAL_DATA_COLLECTOR_H
+#define CAL_DATA_COLLECTOR_H
+
 #include <ros/ros.h>
+
+class CalDataCollector
+{
+public:
+  CalDataCollector(ros::NodeHandle nh, ros::NodeHandle pnh);
+
+  ~CalDataCollector(void) { }
+
+  void collectData(void);
+
+// Private Methods
+private:
+
+// Private Variables
+private:
+};
+
+#endif // CAL_DATA_COLLECTOR_H
--- industrial_calibration/src/cal_data_collector.cpp
+++ industrial_calibration/src/cal_data_collector.cpp
@@ -2,6 +2,10 @@
 
 int main(int argc, char* argv[])
 {
-  
+  ros::init(argc, argv, "cal_data_collector");
+  ros::NodeHandle nh;
+  ros::NodeHandle pnh("~");
+
+  ROS_INFO_STREAM("Hello World!");
   return 0;
 }
