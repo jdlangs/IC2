fb4a404554f096d8faafaecd7e686fcfe0111199
geoffreychiou <geoffrey.chiou@gmail.com>
2017-08-15 16:33:54 -0500

    Removed calibration data collector

--- industrial_calibration_gui/src/calibration_data_collector.cpp
+++
@@ -1,8 +0,0 @@
-#include <industrial_calibration_gui/calibration_data_collector.h>
-namespace industrial_calibration_gui
-{
-void asdf(void)
-{
-  int x  = 0;
-}
-} // namespace industrial_calibration_gui
