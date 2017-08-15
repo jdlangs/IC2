c02c3b3c9627158d1ab707967ed532b3b1898d37
geoffreychiou <geoffrey.chiou@gmail.com>
2017-08-15 16:28:26 -0500

    Added most of the features for the data collection page

--- industrial_calibration_gui/include/industrial_calibration_gui/calibration_widget.h
+++ industrial_calibration_gui/include/industrial_calibration_gui/calibration_widget.h
@@ -2,7 +2,13 @@
 #define CALIBRATION_WIDGET_H
 
 #include <QWidget>
+#include <QFileDialog>
+
 #include <ros/ros.h>
+#include <ros/console.h>
+#include <ui_calibration_widget.h>
+#include <industrial_calibration_libs/industrial_calibration_libs.h>
+#include <industrial_calibration_gui/calibration_data_collector.h>
 
 namespace Ui
 {
@@ -20,17 +26,36 @@ public:
   virtual ~CalibrationWidget();
 
 protected Q_SLOTS:
+  // Start page
   void instructionsCheckbox(void);
   void startCalibrationButton(void);
   void selectCalibrationTypeComboBox(void);
   void updateCalibrationTypeText(int current_index);  
   
+  // Data collection page
+  void outputLocationButton(void);
+  void outputLocationLine(void);
+  void loadTargetLine(void);
+  void loadTargetButton(void);
+  void setTargetLines(const industrial_calibration_libs::Target &target);
+  void setInputsButton(void);
+  bool checkEmptyLines(void);
+  bool checkTarget(const industrial_calibration_libs::TargetDefinition &target_definition);
+
 protected:
   Ui::CalibrationWidget* ui_;
   ros::NodeHandle nh_;
+  ros::NodeHandle pnh_;
 
 private:
+  // GUI variables
   bool instructions_checkbox_state_;
+  QString save_data_directory_;
+  bool target_set_from_file_;
+  bool collecting_data_;
+
+  // Calibration variables
+  industrial_calibration_libs::Target target_;
 };
 } // namespace industrial_calibration_gui
 
--- industrial_calibration_gui/src/calibration_widget.cpp
+++ industrial_calibration_gui/src/calibration_widget.cpp
@@ -1,16 +1,16 @@
-#include <ros/console.h>
 #include <industrial_calibration_gui/calibration_widget.h>
-#include <ui_calibration_widget.h>
 
 namespace industrial_calibration_gui
 {
-CalibrationWidget::CalibrationWidget(QWidget* parent) : QWidget(parent)
+CalibrationWidget::CalibrationWidget(QWidget* parent) : QWidget(parent), pnh_("~")
 {
   // UI setup
   ui_ = new Ui::CalibrationWidget;
   ui_->setupUi(this);
 
   this->instructions_checkbox_state_ = false;
+  this->target_set_from_file_ = false;
+  this->collecting_data_ = true;
   this->updateCalibrationTypeText(ui_->calibration_type_combo_box->currentIndex());
 
   // Start page
@@ -20,6 +20,18 @@ CalibrationWidget::CalibrationWidget(QWidget* parent) : QWidget(parent)
     this, SLOT(startCalibrationButton()));
   connect(ui_->calibration_type_combo_box, SIGNAL(currentIndexChanged(int)), 
     this, SLOT(selectCalibrationTypeComboBox()));
+
+  // Data collection page
+  connect(ui_->output_location_line, SIGNAL(editingFinished()),
+    this, SLOT(outputLocationLine()));
+  connect(ui_->output_location_button, SIGNAL(clicked()),
+    this, SLOT(outputLocationButton()));
+  connect(ui_->load_target_line, SIGNAL(editingFinished()),
+    this, SLOT(loadTargetLine()));
+  connect(ui_->load_target_button, SIGNAL(clicked()),
+    this, SLOT(loadTargetButton()));
+  connect(ui_->set_inputs_button, SIGNAL(clicked()),
+    this, SLOT(setInputsButton()));
 
 }
 
@@ -81,4 +93,204 @@ void CalibrationWidget::updateCalibrationTypeText(int current_index)
   }
 }
 
+void CalibrationWidget::outputLocationLine(void)
+{
+  save_data_directory_ = ui_->output_location_line->text();
+  ROS_INFO_STREAM("Data will be saved to: " << save_data_directory_.toStdString());  
+}
+
+void CalibrationWidget::outputLocationButton(void)
+{
+  save_data_directory_ = QFileDialog::getExistingDirectory(this, tr("Open Directory"),
+    "/home", QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+  ui_->output_location_line->setText(save_data_directory_); 
+  ROS_INFO_STREAM("Data will be saved to: " << save_data_directory_.toStdString());
+}
+
+void CalibrationWidget::loadTargetLine(void)
+{
+  QString target_file = ui_->load_target_line->text();
+  ROS_INFO_STREAM("Loading target from: " << target_file.toStdString());
+  if (target_.loadTargetFromYAML(target_file.toStdString()))
+  {
+    ROS_INFO_STREAM("Target successfully loaded from: " << target_file.toStdString());
+    this->setTargetLines(target_);
+    this->target_set_from_file_ = true;
+  }
+  else
+  {
+    ROS_ERROR_STREAM("Unable to load target from: " << target_file.toStdString());
+  }
+
+}
+
+void CalibrationWidget::loadTargetButton(void)
+{
+  QString target_file = QFileDialog::getOpenFileName(this, tr("Open File"),
+    "/home", tr("YAML Files (*.yaml *.yml)"));
+  ui_->load_target_line->setText(target_file);
+  ROS_INFO_STREAM("Loading target from: " << target_file.toStdString());
+  if (target_.loadTargetFromYAML(target_file.toStdString()))
+  {
+    ROS_INFO_STREAM("Target successfully loaded from: " << target_file.toStdString());
+    this->setTargetLines(target_);
+    this->target_set_from_file_ = true;
+  }
+  else
+  {
+    ROS_ERROR_STREAM("Unable to load target from: " << target_file.toStdString());
+  }  
+}
+
+void CalibrationWidget::setTargetLines(const industrial_calibration_libs::Target &target)
+{
+  // Round numbers with decimals
+  std::stringstream target_circle_diameter_stream;
+  target_circle_diameter_stream << std::fixed << std::setprecision(5) 
+    << target.getData().circle_diameter;
+  std::stringstream target_point_spacing_stream;
+  target_point_spacing_stream << std::fixed << std::setprecision(5)
+    << target.getData().spacing;
+  
+  // Setting line outputs
+  QString target_name = QString::fromStdString(target.getData().target_name);
+  QString target_rows = QString::fromStdString(std::to_string(target.getData().target_rows));
+  QString target_cols = QString::fromStdString(std::to_string(target.getData().target_cols));
+  QString target_points = 
+    QString::fromStdString(std::to_string(target.getData().target_points));
+  QString target_circle_diameter = 
+    QString::fromStdString(target_circle_diameter_stream.str());
+  QString target_point_spacing = 
+    QString::fromStdString(target_point_spacing_stream.str());
+
+  ui_->target_name_line->setText(target_name);
+  ui_->target_name_line->setReadOnly(true);
+  ui_->target_type_combo_box->setCurrentIndex(target.getData().target_type);
+  ui_->target_type_combo_box->setDisabled(true);
+  ui_->target_rows_line->setText(target_rows);
+  ui_->target_rows_line->setReadOnly(true);
+  ui_->target_cols_line->setText(target_cols);
+  ui_->target_cols_line->setReadOnly(true);  
+  ui_->target_points_line->setText(target_points);
+  ui_->target_points_line->setReadOnly(true);
+  ui_->target_circle_diameter_line->setText(target_circle_diameter);
+  ui_->target_circle_diameter_line->setReadOnly(true);
+  ui_->target_point_spacing_line->setText(target_point_spacing);
+  ui_->target_point_spacing_line->setReadOnly(true);
+}
+
+void CalibrationWidget::setInputsButton(void)
+{
+  // Checking if any of the input fields are empty.
+  if (this->checkEmptyLines()) {return;}
+
+  // Generate a target definition if target isn't set from file.
+  if (!target_set_from_file_)
+  {
+    industrial_calibration_libs::TargetDefinition target_definition;
+
+    target_definition.target_name = ui_->target_name_line->text().toStdString();
+    target_definition.target_type 
+      = static_cast<std::size_t>(ui_->target_type_combo_box->currentIndex());
+    target_definition.target_rows 
+      = std::strtoul(ui_->target_rows_line->text().toStdString().c_str(), NULL, 0);
+    target_definition.target_cols
+      = std::strtoul(ui_->target_cols_line->text().toStdString().c_str(), NULL, 0);
+    target_definition.target_points
+      = std::strtoul(ui_->target_points_line->text().toStdString().c_str(), NULL, 0);
+    target_definition.circle_diameter
+      = std::atof(ui_->target_circle_diameter_line->text().toStdString().c_str());
+    target_definition.spacing
+      = std::atof(ui_->target_point_spacing_line->text().toStdString().c_str());
+
+    // Load target definition into target
+    if (this->checkTarget(target_definition))
+    {
+      if (target_.loadTargetFromDefinition(target_definition))
+      {
+        ROS_INFO_STREAM("Successfully loaded target definition of name = " <<
+          target_definition.target_name << ", of type = " 
+          << target_definition.target_type <<
+          ", with rows = " << target_definition.target_rows << ", and cols = " <<
+          target_definition.target_cols << ", and total points = " << 
+          target_definition.target_points << ", with circle diameter = " <<
+          target_definition.circle_diameter << " (m), and point spacing = " <<
+          target_definition.spacing << " (m).");
+      }
+    }
+    else {return;}
+  }
+
+  // Getting data from top and link name inputs
+  std::string base_link = ui_->base_link_line->text().toStdString();
+  std::string tip_link = ui_->tip_link_line->text().toStdString();
+  std::string camera_frame = ui_->camera_calibration_frame_line->text().toStdString();
+  std::string image_topic = ui_->image_topic_line->text().toStdString();
+  std::string camera_info_topic = ui_->camera_info_topic_line->text().toStdString();
+
+  // CalDataCollector cal_data_collector(nh_, pnh_);
+  // cal_data_collector.collectData();
+}
+
+bool CalibrationWidget::checkEmptyLines(void)
+{
+  if (!target_set_from_file_)
+  {
+    if (ui_->target_name_line->text().isEmpty() ||
+      ui_->target_rows_line->text().isEmpty() ||
+      ui_->target_cols_line->text().isEmpty() ||
+      ui_->target_points_line->text().isEmpty() ||
+      ui_->target_circle_diameter_line->text().isEmpty() ||
+      ui_->target_point_spacing_line->text().isEmpty())
+    {
+      ROS_ERROR_STREAM("Please set all target input fields or load a target from a YAML file!");
+      return true;
+    }
+    if (ui_->target_type_combo_box->currentIndex() != 2)
+    {
+      ROS_ERROR_STREAM("The modified circle grid target type is the only " <<
+        "target supported at this time");
+      return true;
+    }
+  }
+  if (ui_->base_link_line->text().isEmpty() ||
+    ui_->tip_link_line->text().isEmpty() ||
+    ui_->camera_calibration_frame_line->text().isEmpty() ||
+    ui_->image_topic_line->text().isEmpty() ||
+    ui_->camera_info_topic_line->text().isEmpty())
+  {
+    ROS_ERROR_STREAM("Please set all link names and topics!");
+    return true;
+  }
+  return false;
+}
+
+bool CalibrationWidget::checkTarget(const 
+  industrial_calibration_libs::TargetDefinition &target_definition)
+{
+  if (target_definition.target_name.empty()) 
+  {
+    ROS_ERROR_STREAM("The 'Target Name' field is empty!");
+    return false;
+  }
+  if (target_definition.target_type != 2) 
+  {
+    ROS_ERROR_STREAM("Target type must be set to Modified Circle Grid!");
+    return false;
+  }
+  if (target_definition.target_rows == 0 || target_definition.target_cols == 0 ||
+    target_definition.target_points == 0 || target_definition.circle_diameter == 0 ||
+    target_definition.spacing == 0) 
+  {
+    ROS_ERROR_STREAM("One of the input fields has a non-numeric value or is set to zero!");
+    return false;
+  }
+  if (target_definition.target_rows*target_definition.target_cols != 
+    target_definition.target_points) 
+  {
+    ROS_ERROR_STREAM("Target rows * cols do not match total number of points!");
+    return false;
+  }
+  return true;
+}
 } // namespace industrial_calibration_gui
