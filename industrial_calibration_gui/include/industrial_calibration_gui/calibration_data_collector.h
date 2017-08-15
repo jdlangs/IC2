8f014968bb465ac09be56e896afcb5fb7d9a7bcb
geoffreychiou <geoffrey.chiou@gmail.com>
2017-08-15 16:28:46 -0500

    Started porting cal_data_collector over to this program

---
+++ industrial_calibration_gui/include/industrial_calibration_gui/calibration_data_collector.h
@@ -0,0 +1,8 @@
+#ifndef CALIBRATION_DATA_COLLECTOR_H
+#define CALIBRATION_DATA_COLLECTOR_H
+
+namespace industrial_calibration_gui
+{
+
+} // namespace industrial_calibration_gui
+#endif // CALIBRATION_DATA_COLLECTOR_H
---
+++ industrial_calibration_gui/src/calibration_data_collector.cpp
@@ -0,0 +1,8 @@
+#include <industrial_calibration_gui/calibration_data_collector.h>
+namespace industrial_calibration_gui
+{
+void asdf(void)
+{
+  int x  = 0;
+}
+} // namespace industrial_calibration_gui
