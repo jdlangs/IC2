15b95bd17e760f8dbbd6dc1d0d3f7a477922dc76
geoffreychiou <geoffrey.chiou@gmail.com>
2017-10-13 14:08:59 -0500

    copied over helper functions from industrial_calibration examples

--- industrial_calibration_libs/test/test_utils.h
+++ industrial_calibration_libs/test/test_utils.h
@@ -98,12 +98,23 @@ parseYAML(const YAML::Node &node, const std::string &var_name,
   std::vector<double> &var_value);
 
 static bool 
-loadLinkData(const std::size_t &index, const std::string &path,
-  LinkData *link_data);
+loadLinkData(const std::string &file_path, LinkData *link_data, 
+  const std::string &node);
+
+static void 
+convertToPose6D(const LinkData &link_data, 
+  industrial_calibration_libs::Pose6D &link_pose);
 
 static bool 
 convertToPose6D(const std::vector<LinkData> &link_data, 
   std::vector<industrial_calibration_libs::Pose6D> *link_poses);
+
+static void 
+getQuaternion(double distance, double &qx, double &qy, double &qz,
+  double &qw);
+
+static bool 
+loadCameraInfo(const std::string &path, double *camera_info);
 
 static bool 
 parseYAML(const YAML::Node &node, const std::string &var_name, 
@@ -126,23 +137,38 @@ parseYAML(const YAML::Node &node, const std::string &var_name,
 }
 
 static bool 
-loadLinkData(const std::size_t &index, const std::string &path,
-  LinkData *link_data)
+loadLinkData(const std::string &file_path, LinkData *link_data,
+  const std::string &node)
 {
   bool success = true;
-  std::string file_path = path + std::to_string(index) + ".yaml";
 
   YAML::Node data_yaml;
   try
   {
     data_yaml = YAML::LoadFile(file_path);
-    if (!data_yaml["base_link_to_tool0"]) {return false;}
+    if (!data_yaml[node]) {return false;}
   }
   catch (YAML::BadFile &bf) {return false;}
 
-  success &= parseYAML(data_yaml["base_link_to_tool0"], "Translation", link_data->translation);
-  success &= parseYAML(data_yaml["base_link_to_tool0"], "Quaternion", link_data->rotation_quat);
+  success &= parseYAML(data_yaml[node], "Translation", link_data->translation);
+  success &= parseYAML(data_yaml[node], "Quaternion", link_data->rotation_quat);
   return success;
+}
+
+static void 
+convertToPose6D(const LinkData &link_data, 
+  industrial_calibration_libs::Pose6D &link_pose)
+{
+  double tx = link_data.translation[0];
+  double ty = link_data.translation[1];
+  double tz = link_data.translation[2];
+  double qx = link_data.rotation_quat[0];
+  double qy = link_data.rotation_quat[1];
+  double qz = link_data.rotation_quat[2];
+  double qw = link_data.rotation_quat[3];
+
+  link_pose.setOrigin(tx, ty, tz);
+  link_pose.setQuaternion(qx, qy, qz, qw);  
 }
 
 static bool 
@@ -154,15 +180,8 @@ convertToPose6D(const std::vector<LinkData> &link_data,
   for (std::size_t i = 0; i < link_data.size(); i++)
   {
     industrial_calibration_libs::Pose6D link_pose;
-    double tx = link_data[i].translation[0];
-    double ty = link_data[i].translation[1];
-    double tz = link_data[i].translation[2];
-    double qx = link_data[i].rotation_quat[0];
-    double qy = link_data[i].rotation_quat[1];
-    double qz = link_data[i].rotation_quat[2];
-    double qw = link_data[i].rotation_quat[3];
-    link_pose.setOrigin(tx, ty, tz);
-    link_pose.setQuaternion(qx, qy, qz, qw);
+
+    convertToPose6D(link_data[i], link_pose);
 
     link_poses->push_back(link_pose);
   }
@@ -171,6 +190,54 @@ convertToPose6D(const std::vector<LinkData> &link_data,
   else {return false;}
 }
 
+static void 
+getQuaternion(double distance, double &qx, double &qy, double &qz,
+  double &qw)
+{
+  Eigen::Matrix3d m;
 
+  m(0,0) =  1; m(0,1) =   0; m(0,2) =  0;
+  m(1,0) =  0; m(1,1) =  -1; m(1,2) =  0;
+  m(2,0) =  0; m(2,1) =   0; m(2,2) = -1;
 
+  industrial_calibration_libs::Pose6D temp_pose;
+  temp_pose.setBasis(m);
+  temp_pose.setOrigin(-0.1, -0.1, distance);
+  temp_pose.getQuaternion(qx, qy, qz, qw);   
+}
+
+static bool 
+loadCameraInfo(const std::string &path, double *camera_info)
+{
+  bool success = true;
+
+  YAML::Node camera_info_yaml;
+  try
+  {
+    camera_info_yaml = YAML::LoadFile(path);
+    if (!camera_info_yaml["camera_matrix"]) {return false;}
+    if (!camera_info_yaml["distortion_coefficients"]) {return false;}
+  }
+  catch (YAML::BadFile &bf) {return false;}
+
+  std::vector<double> camera_matrix;
+  std::vector<double> distortion_coefficients;
+
+  success &= parseYAML(camera_info_yaml["camera_matrix"], "data", camera_matrix);
+  success &= parseYAML(camera_info_yaml["distortion_coefficients"], 
+    "data", distortion_coefficients);
+
+  // Assuming it is formatted correctly
+  camera_info[0] = camera_matrix[0]; // focal length x
+  camera_info[1] = camera_matrix[4]; // focal_length y
+  camera_info[2] = camera_matrix[2]; // optical center x
+  camera_info[3] = camera_matrix[5]; // optical center y
+  camera_info[4] = distortion_coefficients[0]; // distortion k1
+  camera_info[5] = distortion_coefficients[1]; // distortion k2
+  camera_info[6] = distortion_coefficients[2]; // distortion k3
+  camera_info[7] = distortion_coefficients[3]; // distortion p1
+  camera_info[8] = distortion_coefficients[4]; // distortion p2
+
+  return success;
+}
 #endif // TEST_UTILS_H
