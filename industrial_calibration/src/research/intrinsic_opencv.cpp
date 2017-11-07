7eb32f8fe0eec3dee79ba01706375babab4bd37d
geoffreychiou <geoffrey.chiou@gmail.com>
2017-11-07 02:50:27 -0600

    Added data_dir parameter

--- industrial_calibration/src/research/intrinsic_opencv.cpp
+++ industrial_calibration/src/research/intrinsic_opencv.cpp
@@ -306,12 +306,17 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "intrinsic_opencv");
   ros::NodeHandle pnh("~");
 
+  std::string data_dir;
+  pnh.getParam("data_dir", data_dir);
+  data_dir = addSlashToEnd(data_dir);
+
   std::vector<std::string> data_sets = { "01", "02", "03", "04", "05", 
     "06", "07", "08", "09", "10", "11", "12", "13", "14", "15" };
-  std::string data_dir = "/home/gchiou/UTSA/Thesis/thesis_data/";
-  for (std::size_t i = 0; i < 15; i++)
+
+  for (std::size_t i = 0; i < data_sets.size(); i++)
   {
     calibrateDataSet(data_dir, data_sets[i]);
   }
+
   return 0;
 }
