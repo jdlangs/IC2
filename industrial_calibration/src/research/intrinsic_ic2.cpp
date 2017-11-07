08c039e4fc12e640031649e07ef0730310a3c472
geoffreychiou <geoffrey.chiou@gmail.com>
2017-11-07 02:34:05 -0600

    Added boilerplate research code for IC2

---
+++ industrial_calibration/src/research/intrinsic_ic2.cpp
@@ -0,0 +1,7 @@
+#include <ros/ros.h>
+#include <yaml-cpp/yaml.h>
+#include <industrial_calibration_libs/industrial_calibration_libs.h>
+#include <industrial_calibration/helper_functions.h>
+
+#include <opencv2/core/core.hpp>
+#include <fstream>
