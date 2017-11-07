6c70b4c62cf6ba37f061543dc428d00d79d24762
geoffreychiou <geoffrey.chiou@gmail.com>
2017-11-07 03:09:34 -0600

    added files for research intrinsic

---
+++ industrial_calibration_libs/include/industrial_calibration_libs/calibration_types/research_intrinsic.h
@@ -0,0 +1,8 @@
+#ifndef RESEARCH_INTRINSIC_H
+#define RESEARCH_INTRINSIC_H
+
+#include <industrial_calibration_libs/calibration.h>
+namespace industrial_calibration_libs
+{
+} // namespace industrial_calibration_libs
+#endif // RESEARCH_INTRINSIC_H
---
+++ industrial_calibration_libs/src/calibration_types/research_intrinsic.cpp
@@ -0,0 +1,1 @@
+#include <industrial_calibration_libs/calibration_types/research_intrinsic.h>
